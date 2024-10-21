506299637ada292a83263f3f76a01846e28e5165
Anuhya Talamanchi <anuhya92@gmail.com>
2024-10-21 22:36:46 +0200

    fix reveiw comment

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let priceFromUser = prompt("Enter price", "0");
-let priceInNbr = parseFloat(priceFromUser);
+let priceFromUser1 = prompt("Enter price", "0");
+let priceFromUser2 = priceFromUser1.slice(1);
+let priceInNbr = parseFloat(priceFromUser2);
 let discount = (priceInNbr * 10) / 100;
 let newPrice = priceInNbr - discount;
 console.log(`New price: $${newPrice}`);
